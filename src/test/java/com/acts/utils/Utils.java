5d728514b80c7e27bca9a63d4fb3d900f4502fb1
Nishant Nikam <nikamn@gmx.com>
2024-01-15 17:37:09 +0000

    updated properties

--- src/test/java/com/acts/utils/Utils.java
+++ src/test/java/com/acts/utils/Utils.java
@@ -9,21 +9,22 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.Select;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
+//import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Utils {
     // get webdriver instance
     public static WebDriver getWebDriver() {
-        System.setProperty("webdriver.chrome.driver", "/home/nishant/.cache/selenium/chromedriver/linux64/120.0.6099.109/chromedriver");
+        // System.setProperty("webdriver.chrome.driver", "/home/nishant/.cache/selenium/chromedriver/linux64/120.0.6099.109/chromedriver");
         ChromeOptions chromeOptions = new ChromeOptions();
         chromeOptions.addArguments("--disable-dev-shm-usage");
-        // chromeOptions.addArguments("--headless");
-        chromeOptions.addArguments("--disable-gpu");
-        chromeOptions.addArguments("--no-sandbox");
+        // // chromeOptions.addArguments("--headless");
+        // chromeOptions.addArguments("--disable-gpu");
+        // chromeOptions.addArguments("--no-sandbox");
 
-        WebDriverManager.chromedriver().setup();
+        // WebDriverManager.chromedriver().setup();
 
         WebDriver driver = new ChromeDriver(chromeOptions);
+
 
         return driver;
     }
